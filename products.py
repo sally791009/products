f020596e67509c95a35e43836aa89e20f7fdd155
SALI MU <sally791009@gmail.com>
2020-05-12 14:10:56 +0800

    add exist file

--- products.py
+++ products.py
@@ -1,12 +1,20 @@
+
+import os #operation system
 #讀取檔案
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #跳過並繼續到下一迴
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+
+if os.path.isfile('products.csv'): #檢察檔案在不在
+	print('yeah!找到檔案')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #跳過並繼續到下一迴
+			name, price = line.strip().split(',') #spilt用逗點分隔成小清單
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案')
+
 #讓使用者輸入
 while True:
 	name = input('請輸入商品名稱')
@@ -16,9 +24,11 @@ while True:
 	price = int(price)
 	products.append([name, price]) #建立二維清單，直接在這建立小清單
 print(products)
+
 #印出購買紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
+
 #寫入檔案
 with open ('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n') #寫入欄位名稱 逗點表示分欄
